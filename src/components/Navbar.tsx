fd9c6660bf9ddbe520b03cb0c9002b905b9066b4
rosh2004 <roshaan20043@gmail.com>
2024-12-19 00:14:51 +0500

    hid links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
 
   const links = [
     { href: '/', title: 'Home' },
-    { href: '/blogs', title: 'Blogs' },
+    // { href: '/blogs', title: 'Blogs' },
   ];
   const renderedLinks = links.map((link, index) => {
     return (
@@ -22,9 +22,9 @@ function Navbar() {
   return (
     <header className="min-h-[65px] bg-white/95 dark:bg-black/95 fixed w-full z-10">
       <nav className="flex justify-between items-center py-3 mx-auto px-4">
-        <Toggle className="sm:hidden mr-6 md:mr-0 text-primary hover:opacity-70 hover:bg-accent hover:text-foreground-light dark:hover:bg-accent dark:hover:text-foreground-light data-[state=on]:bg-accent dark:data-[state=on]:bg-accent data-[state=on]:text-foreground-light dark:data-[state=on]:text-foreground-light">
+        {/* <Toggle className="sm:hidden mr-6 md:mr-0 text-primary hover:opacity-70 hover:bg-accent hover:text-foreground-light dark:hover:bg-accent dark:hover:text-foreground-light data-[state=on]:bg-accent dark:data-[state=on]:bg-accent data-[state=on]:text-foreground-light dark:data-[state=on]:text-foreground-light">
           <GiHamburgerMenu className="scale-150"/>
-        </Toggle >
+        </Toggle > */}
         <Link href='/' className="flex items-center">
           <MainLogo className="sm:h-10 h-9" height={'100%'}/>
         </Link>
