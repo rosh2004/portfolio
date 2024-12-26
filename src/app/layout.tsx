62689c7769e35406e40cdbd7b607fd4a951cd94b
rosh2004 <roshaan20043@gmail.com>
2024-12-26 14:59:13 +0500

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter, Roboto } from "next/font/google";
 import Navbar from "../components/Navbar";
 import DownloadResumeButton from "@/components/DownloadResumeButton";
@@ -42,6 +43,7 @@ export default function RootLayout({
         </div>
         <DownloadResumeButton />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
